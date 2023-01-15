655f87ac97e676558e4de04edeb763f823cf945b
Trison Nguyen <trison.ng@gmail.com>
2023-01-14 18:30:30 -0800

    handle serving react client files

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
+const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const exps = express();
 
+
+// Access React client files
+exps.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle API requests
 exps.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+exps.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+// Listen on port
 exps.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
