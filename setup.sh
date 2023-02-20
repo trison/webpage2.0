5165401bc57ad8b9be5432915a28318e77bb57a8
Trison Nguyen <trison.ng@gmail.com>
2023-02-20 09:21:38 -0800

    add setup script for new envs

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Fix common issues on new dev environments
+# TODO: dockerize or use npm shrinkwrap
+
+# Remove old packages
+rm -rf node_modules
+rm package-lock.json
+
+# Explicitly use Node 18
+npm install react@18 react-dom@18
+
+# Install missing package
+npm install --save-dev web-vitals
